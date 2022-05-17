954e95b2a5dda5b9743af63b6330dad31d2f312c
Jonathan Bereyziat <jon.jbstar@gmail.com>
2022-05-17 09:02:26 +0200

    MUI integration

--- lf-mui-poc/pages/index.tsx
+++ lf-mui-poc/pages/index.tsx
@@ -1,85 +1,69 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Stack from '@mui/material/Stack'
+import Paper from '@mui/material/Paper'
+import { styled } from '@mui/material/styles'
+import ImgCard from '../components/ImgCard'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import { StyledEngineProvider } from '@mui/material/styles'
+
+import TextField from '@mui/material/TextField'
+import LoginField from '../components/LoginField'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}))
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <StyledEngineProvider injectFirst>
+      <Container maxWidth="xl">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Grid
+          justifyContent="center"
+          alignItems="center"
+          sx={{ flexGrow: 1, height: '100vh' }}
+          container
+          spacing={2}
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+          <Grid item xs={12} md={6}>
+            <Grid item>
+              <Typography gutterBottom variant="h2" component="div">
+                Welcome back <br></br> to Logifleet 360°
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography gutterBottom variant="body1" component="div">
+                Log in to your Logifleet 360° account
+              </Typography>
+            </Grid>
+            <LoginField />
+
+            <Grid item>
+              <Typography gutterBottom variant="body1" component="div">
+                Don't have an account yet? Contact us!
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={6} sx={{ display: { md: 'block', xs: 'none' } }}>
+            <Grid container justifyContent="center">
+              <ImgCard />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </StyledEngineProvider>
   )
 }
 
